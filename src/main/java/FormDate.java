cb306d3f8cb6846580a50de093824d94f0dceea2
Neto test <demon_4@mail.ru>
2023-09-10 14:26:36 +0300

    First commit

--- src/main/java/FormDate.java
+++ src/main/java/FormDate.java
@@ -1,5 +1,5 @@
 public class FormDate {
-        public String day;
-        public String month;
-        public String year;
+    public String day;
+    public String month;
+    public String year;
 }
